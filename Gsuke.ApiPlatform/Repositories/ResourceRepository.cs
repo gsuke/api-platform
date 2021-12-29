39b52f8a3110753085f1ec8fcf2a05f59c5dfb5c
gsuke <54441258+gsuke@users.noreply.github.com>
2021-12-29 18:44:19 +0900

    Repositoryでも設定ファイルを読み込めるように

--- Gsuke.ApiPlatform/Repositories/ResourceRepository.cs
+++ Gsuke.ApiPlatform/Repositories/ResourceRepository.cs
@@ -1,13 +1,19 @@
-using System;
-using Gsuke.ApiPlatform.Repositories;
-
 namespace Gsuke.ApiPlatform.Repositories
 {
     public class ResourceRepository : IResourceRepository
     {
+        private readonly ILogger<IResourceRepository> _logger;
+        private readonly IConfiguration _config;
+
+        public ResourceRepository(ILogger<IResourceRepository> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
         public string GetResourceList()
         {
-            return "Hello";
+            return _config["RdbConnectionStrings"];
         }
     }
 }
