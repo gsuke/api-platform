e0256bc8abff0e5d7babbcb51f5d3e330fa9371b
gsuke <54441258+gsuke@users.noreply.github.com>
2021-12-29 15:58:53 +0900

    appsettingsの値を参照してみた

--- Gsuke.ApiPlatform/Controllers/ManageController.cs
+++ Gsuke.ApiPlatform/Controllers/ManageController.cs
@@ -6,27 +6,18 @@ namespace Gsuke.ApiPlatform.Controllers;
 [Route("[controller]")]
 public class ManageController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IConfiguration _config;
 
-    public ManageController(ILogger<WeatherForecastController> logger)
+    public ManageController(ILogger<WeatherForecastController> logger, IConfiguration config)
     {
         _logger = logger;
+        _config = config;
     }
 
     [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
+    public string Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return _config["Logging:LogLevel:Default"];
     }
 }
