202cfc5dfa71f7500bc0a0221f020b8d7e9eaba0
gsuke <54441258+gsuke@users.noreply.github.com>
2022-01-01 17:23:59 +0900

    テーブル定義を変更, Dapperマッピングを改善

--- docker/ddl.sql
+++ docker/ddl.sql
@@ -1,16 +1,24 @@
 DROP TABLE IF EXISTS resources;
+
 CREATE TABLE IF NOT EXISTS resources (
-    id uuid PRIMARY KEY,
-    url varchar(32)
+    url varchar(64) PRIMARY KEY
+    , data_schema varchar(8192)
 );
+
 INSERT
     INTO
-    resources (
-        id
-        , url
-    )
+    resources
 VALUES
 (
-    '8771a522-ca5c-fe20-a745-56d5e5bfee01'
-    , 'hello'
-)
+    'markets'
+    , 'test-dataschema1'
+);
+
+INSERT
+    INTO
+    resources
+VALUES
+(
+    'wish-list'
+    , 'test-dataschema2'
+);
