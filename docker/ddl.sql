4051bcec290d5ddb8af2176356ee1a19b9268acc
gsuke <54441258+gsuke@users.noreply.github.com>
2022-01-02 17:23:07 +0900

    使っていなかったDB資源をすべて削除

--- docker/ddl.sql
+++ docker/ddl.sql
@@ -4,21 +4,3 @@ CREATE TABLE IF NOT EXISTS resources (
     url varchar(64) PRIMARY KEY
     , data_schema varchar(8192)
 );
-
-INSERT
-    INTO
-    resources
-VALUES
-(
-    'markets'
-    , 'test-dataschema1'
-);
-
-INSERT
-    INTO
-    resources
-VALUES
-(
-    'wish-list'
-    , 'test-dataschema2'
-);
