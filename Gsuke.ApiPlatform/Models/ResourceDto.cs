aaf9f490ae6ccd81316db5df2e3ea59e952a14db
gsuke <54441258+gsuke@users.noreply.github.com>
2022-01-09 15:19:25 +0900

    Resourceにバリデーション条件を追加

--- Gsuke.ApiPlatform/Models/ResourceDto.cs
+++ Gsuke.ApiPlatform/Models/ResourceDto.cs
@@ -5,8 +5,11 @@ namespace Gsuke.ApiPlatform.Models
     public class ResourceDto
     {
         [Required]
+        [MaxLength(64)]
+        [RegularExpression(@"[a-zA-Z0-9]+", ErrorMessage = "Characters are not allowed.")]
         public string? url { get; set; }
         [Required]
+        [MaxLength(8192)]
         public string? dataSchema { get; set; }
     }
 }
